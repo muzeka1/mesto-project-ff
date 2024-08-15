47c3fc77759de02041cb8d05a933cae659cd8f72
Abdulla Khalitov <abdullahalitoff@gmail.com>
2024-08-15 16:13:55 +0300

    features/cards_initialisation cards_removing

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,24 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+
+const cardTemplate = document.querySelector('#card-template').content;
+const cardsContainer = document.querySelector('.places__list');
+
+function cardInit(cardInfo) {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    cardElement.querySelector('.card__image').src = cardInfo.link;
+    cardElement.querySelector('.card__image').alt = cardInfo.name;
+    cardElement.querySelector('.card__title').textContent = cardInfo.name;
+    cardElement.querySelector('.card__delete-button').addEventListener('click', () => {cardDelete(cardElement)})
+    return cardElement
+}
+
+function cardDelete(cardElement) {
+    cardElement.remove()
+}
+
+initialCards.forEach(cardInfo => {
+    cardsContainer.append(cardInit(cardInfo));
+})

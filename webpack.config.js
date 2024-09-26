2888ad212134b2be510de8525f3b876f9f75e015
Abdulla Khalitov <abdullahalitoff@gmail.com>
2024-09-26 20:20:24 +0300

    webpack-config/build/folders-for-types

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,66 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    entry: { main: './src/components/index.js'},
-    output: {
-        path: path.resolve(__dirname,'dist'),
-        filename: 'main.js',
-            publicPath: ''
-    },
-    mode: 'development',
-    devServer: {
-        static: path.resolve(__dirname, './dist'), // путь, куда "смотрит" режим разработчика
-        compress: true, // это ускорит загрузку в режиме разработки
-        port: 8080, // порт, чтобы открывать сайт по адресу localhost:8080, но можно поменять порт
-    
-        open: true // сайт будет открываться сам при запуске npm run dev
+  entry: { main: "./src/components/index.js" },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+    publicPath: "",
+  },
+  mode: "development",
+  devServer: {
+    static: path.resolve(__dirname, "./dist"), // путь, куда "смотрит" режим разработчика
+    compress: true, // это ускорит загрузку в режиме разработки
+    port: 8080, // порт, чтобы открывать сайт по адресу localhost:8080, но можно поменять порт
+
+    open: true, // сайт будет открываться сам при запуске npm run dev
+  },
+  module: {
+    rules: [
+      // rules — это массив правил
+      // добавим в него объект правил для бабеля
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+        exclude: "/node_modules/",
       },
-      module: {
-        rules: [ // rules — это массив правил
-          // добавим в него объект правил для бабеля
-          {
-            test: /\.js$/,
-            use: 'babel-loader',
-            exclude: '/node_modules/'
-          },
-          {
-            test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
-            type: 'asset/resource',
-          },
-          {
-            test: /\.css$/,
-            use: [MiniCssExtractPlugin.loader, {
-                loader: 'css-loader',
-                options: {
-                  importLoaders: 1
-                }
-              },
-              'postcss-loader'
-            ]
-          },
-          ]
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "images/[name].[hash][ext]",
+        },
       },
-      plugins: [
-        new HtmlWebpackPlugin({
-          template: './src/index.html' // путь к файлу index.html
-        }),
-        new CleanWebpackPlugin(),
-        new MiniCssExtractPlugin(),
-      ] 
-}
+      {
+        test: /\.(woff(2)?|eot|ttf|otf)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "fonts/[name].[hash][ext]",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+            },
+          },
+          "postcss-loader",
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html", // путь к файлу index.html
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+  ],
+};
